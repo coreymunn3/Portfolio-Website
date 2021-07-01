9bf3c7f5d6b6b4218be8a2707e96c3cf6ff242de
Michael Corey Munn <coreymunn3@gmail.com>
2021-07-01 11:27:48 -0400

    Created SlideIntoView component to animate sections in using intersection observer.

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -1,39 +1,42 @@
 import { Box, Flex, Stack, Heading, Text, Image } from '@chakra-ui/react';
+import SlideIntoView from './SlideIntoView';
 
 const AboutMe = () => {
   return (
-    <Box mt={[100, 250]}>
-      <Flex direction={['column', 'row']} w='100%'>
-        <Stack maxW={['100%', '60%']}>
-          <Heading size='2xl'>About Me</Heading>
-          <Text>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac
-            consectetur nulla. Integer sit amet odio ante. Etiam dolor metus,
-            tristique dapibus dui eget, tincidunt dictum urna. Cras id ante sed
-            est consectetur consectetur. Sed aliquam ornare convallis. Donec
-            massa nulla, condimentum non laoreet vitae, eleifend ut urna.
-            Vivamus sagittis, urna iaculis suscipit commodo, massa erat tempus
-            lacus, non auctor metus leo quis neque. Sed et tincidunt nibh. Etiam
-            vitae erat in elit posuere convallis eget vel ligula.
-          </Text>
-          <Text>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac
-            consectetur nulla. Integer sit amet odio ante. Etiam dolor metus,
-            tristique dapibus dui eget, tincidunt dictum urna.
-          </Text>
-        </Stack>
-        <Flex justifyContent='center' alignItems='flex-start' w='100%' mt={4}>
-          <Image
-            borderRadius='full'
-            objectFit='cover'
-            w={250}
-            h={250}
-            src='/images/headshot.jpg'
-            alt='corey munn'
-          />
+    <SlideIntoView direction='fromBottom'>
+      <Box>
+        <Flex direction={['column', 'row']} w='100%'>
+          <Stack maxW={['100%', '60%']}>
+            <Heading size='2xl'>About Me</Heading>
+            <Text>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac
+              consectetur nulla. Integer sit amet odio ante. Etiam dolor metus,
+              tristique dapibus dui eget, tincidunt dictum urna. Cras id ante
+              sed est consectetur consectetur. Sed aliquam ornare convallis.
+              Donec massa nulla, condimentum non laoreet vitae, eleifend ut
+              urna. Vivamus sagittis, urna iaculis suscipit commodo, massa erat
+              tempus lacus, non auctor metus leo quis neque. Sed et tincidunt
+              nibh. Etiam vitae erat in elit posuere convallis eget vel ligula.
+            </Text>
+            <Text>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac
+              consectetur nulla. Integer sit amet odio ante. Etiam dolor metus,
+              tristique dapibus dui eget, tincidunt dictum urna.
+            </Text>
+          </Stack>
+          <Flex justifyContent='center' alignItems='flex-start' w='100%' mt={4}>
+            <Image
+              borderRadius='full'
+              objectFit='cover'
+              w={250}
+              h={250}
+              src='/images/headshot.jpg'
+              alt='corey munn'
+            />
+          </Flex>
         </Flex>
-      </Flex>
-    </Box>
+      </Box>
+    </SlideIntoView>
   );
 };
 
---
+++ components/SlideIntoView.js
@@ -0,0 +1,65 @@
+import { motion, useAnimation } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
+import { useEffect } from 'react';
+
+const SlideIntoView = ({ children, direction, pctVisibleThreshold }) => {
+  const controls = useAnimation();
+  const [ref, inView] = useInView({
+    threshold: pctVisibleThreshold ? pctVisibleThreshold : 0.3,
+  });
+
+  useEffect(() => {
+    if (inView) {
+      controls.start('visible');
+    }
+  }, [controls, inView]);
+
+  // calculate starting positions
+  let xStart, yStart;
+  switch (direction) {
+    case 'fromTop':
+      xStart = 0;
+      yStart = -25;
+      break;
+    case 'fromBottom':
+      xStart = 0;
+      yStart = 25;
+      break;
+    case 'fromLeft':
+      xStart = -25;
+      yStart = 0;
+      break;
+    case 'fromRight':
+      xStart = 25;
+      yStart = 0;
+      break;
+  }
+
+  const containerVariants = {
+    hidden: {
+      opacity: 0,
+      y: yStart,
+      x: xStart,
+    },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        delay: 0.2,
+        duration: 0.2,
+      },
+    },
+  };
+  return (
+    <motion.div
+      variants={containerVariants}
+      ref={ref}
+      animate={controls}
+      initial='hidden'
+    >
+      {children}
+    </motion.div>
+  );
+};
+
+export default SlideIntoView;
