0a7b5ff0cea501a07ac0e147a1e855c0da082958
Michael Corey Munn <coreymunn3@gmail.com>
2021-07-01 18:32:43 -0400

    Placed AboutMe Image into SVG for interesting effect.

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -25,14 +25,33 @@ const AboutMe = () => {
             </Text>
           </Stack>
           <Flex justifyContent='center' alignItems='flex-start' w='100%' mt={4}>
-            <Image
+            {/* <Image
               borderRadius='full'
               objectFit='cover'
               w={250}
               h={250}
               src='/images/headshot.jpg'
               alt='corey munn'
-            />
+            /> */}
+            <svg viewBox='25 0 193 190' xmlns='http://www.w3.org/2000/svg'>
+              <defs>
+                <clipPath id='user-space' clipPathUnits='userSpaceOnUse'>
+                  <path
+                    fill='#FF0066'
+                    d='M62.1,-40.2C69.7,-22.6,57.8,2,44.2,26C30.6,50,15.3,73.4,-2.2,74.7C-19.8,76,-39.5,55.1,-53.9,30.7C-68.2,6.2,-77.2,-21.7,-67.3,-40.7C-57.4,-59.6,-28.7,-69.4,-0.8,-69C27.2,-68.5,54.4,-57.8,62.1,-40.2Z'
+                    transform='translate(120 80) scale(1.1)'
+                  />
+                </clipPath>
+              </defs>
+
+              <image
+                width='100%'
+                height='100%'
+                preserveAspectRatio='xMidYMid slice'
+                xlinkHref='/images/headshot.jpg'
+                clipPath='url(#user-space)'
+              />
+            </svg>
           </Flex>
         </Flex>
       </Box>
