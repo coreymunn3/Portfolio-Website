58bddb807c1b454b7c4dc27377f3ea9339cdeeb3
Michael Corey Munn <coreymunn3@gmail.com>
2021-08-01 16:34:20 -0400

    Added project icon for timeline project events; fixed poor spacing in timeline areas.

--- components/TimelineSection.js
+++ components/TimelineSection.js
@@ -1,21 +1,24 @@
 import { Stack, Divider, Text } from '@chakra-ui/react';
+import { Fragment } from 'react';
 import TimelineSectionEvent from './TimelineSectionEvent';
 
 const TimelineSection = ({ year, timelineEvents }) => {
   return (
-    <Stack>
+    <Stack spacing={0}>
       <Divider></Divider>
-      <Text as='h3' fontWeight='bold'>
-        {year}
-      </Text>
-      {timelineEvents.map((event, idx) => (
-        <TimelineSectionEvent
-          event={event}
-          key={idx}
-          // direction={idx % 2 === 0 ? 'fromLeft' : 'fromRight'}
-          direction={'fromRight'}
-        />
-      ))}
+      <Stack spacing={4}>
+        <Text as='h3' fontWeight='bold' color='brand.400'>
+          {year}
+        </Text>
+        {timelineEvents.map((event, idx) => (
+          <TimelineSectionEvent
+            event={event}
+            key={idx}
+            // direction={idx % 2 === 0 ? 'fromLeft' : 'fromRight'}
+            direction={'fromRight'}
+          />
+        ))}
+      </Stack>
     </Stack>
   );
 };
--- components/TimelineSectionEvent.js
+++ components/TimelineSectionEvent.js
@@ -1,19 +1,26 @@
 import { Stack, Flex, Text, Box } from '@chakra-ui/react';
 import SlideIntoView from './SlideIntoView';
 import { IoBriefcase, IoSchool } from 'react-icons/io5';
+import { FaCode } from 'react-icons/fa';
 
 const TimelineSectionEvent = ({ event, direction }) => {
+  const determineTimelineIcon = (category) => {
+    switch (category) {
+      case 'work':
+        return <IoBriefcase size='1rem' />;
+      case 'school':
+        return <IoSchool size='1rem' />;
+      case 'project':
+        return <FaCode size='1rem' />;
+    }
+  };
   return (
     <SlideIntoView direction={direction} pctVisibleThreshold={1}>
       <Flex>
         <Box pl={2} pr={2} pt={1}>
-          {event.fields.category === 'work' ? (
-            <IoBriefcase color='green.200' />
-          ) : (
-            <IoSchool color='green.200' />
-          )}
+          {determineTimelineIcon(event.fields.category)}
         </Box>
-        <Stack>
+        <Stack spacing={0}>
           <Text variant='lead'>{event.fields.title}</Text>
           <Text>{event.fields.description}</Text>
         </Stack>
